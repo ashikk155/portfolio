4f7dea420cef135e61f2ec5e49a5d950ddbc1535
Ashik Tomy <ashiktomy155@gmail.com>
2026-04-02 11:25:03 +0530

    chore: revert github pages setup, prepare for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let repo = '';
-if (isGithubActions && process.env.GITHUB_REPOSITORY) {
-  repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-}
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isGithubActions ? `/${repo}` : "",
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
